d526c3ed53a9d1b691789bea19ec12601aeaf044
erikor12 <erikburs78@gmail.com>
2025-09-08 17:06:08 -0300

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/navbar'
+import Home from './pages/home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/productos" element={<Productos />} />
+        <Route path="/productos/:id" element={<ProductDetail />} /> */}
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+      </Routes>
     </>
   )
 }
---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to ToysRmac!</h1>
+            <p>Your one-stop shop for all things toys.</p>
+        </div>
+    );
+}
+
+export default Home;
