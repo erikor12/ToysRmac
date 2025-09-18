5d8a2a065184e614deadb48458cf69d7381833bf
Andypoma09 <totolinocruz@gmail.com>
2025-09-18 10:46:43 -0300

    Se pudo centrar home. Todavia falta todo lo demas.

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -4,14 +4,14 @@ import "../home/home.css";
 function Home() {
     return (
         <div className="paddingTop">
-            <div>
+            <div className="homeContent">
                 <h1 className="homeh1">Welcome to ToysRmac!</h1>
                 <p className="homep">Your one-stop shop for all things toys.</p>
+                <nav>
+                    <Link className="homebk" to="/bktoys">Burger King Toys</Link>
+                    <Link className="homemc" to="/mctoys">McDonald's Toys</Link>
+                </nav>
             </div>
-            <nav>
-                <Link className="homebk" to="/bktoys">Burger King Toys</Link>
-                <Link className="homemc" to="/mctoys">McDonald's Toys</Link>
-            </nav>
         </div>
     );
 }
