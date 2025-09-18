2b90244992f2109cc4eb2111511fff75eac8ea01
erikor12 <erikburs78@gmail.com>
2025-09-18 11:05:59 -0300

    Reestructuracion parte 1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,12 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import NavBar from './components/navbar/navbar'
 import Home from './pages/home/home'
-import AboutPage from './pages/about'
-import NotFoundPage from './pages/notfound'
-import BKToysPage from './pages/bktoys'
-import MCToysPage from './pages/mctoys'
+import AboutPage from './pages/about/about'
+import NotFoundPage from './pages/notfound/notfound'
+import BKToysPage from './pages/products/bktoys/bktoys'
+import MCToysPage from './pages/products/mctoys/mctoys'
 import Cart from './components/cart/cart'
-import ProductDetail from './pages/productdetails'
+import ProductDetail from './pages/products/productdetails/productdetails'
 
 function App() {
 
